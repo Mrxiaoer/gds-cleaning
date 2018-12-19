ef14ebdd963ac1eb6248c7091c8cf6351f523aad
yaonuan <806039077@qq.com>
2018-12-19 09:33:54 +0800

    测试用例转移

---
+++ dips-modules/gds-cleaning/gds-cleaning-biz/src/test/java/com/cloud/gds/cleaning/service/DataRuleServiceTest.java
@@ -0,0 +1,94 @@
+package com.cloud.gds.cleaning.service;
+
+import com.cloud.gds.cleaning.GdsCleaningApplication;
+import com.cloud.gds.cleaning.api.entity.DataRule;
+import com.cloud.gds.cleaning.api.vo.DataRuleVo;
+import com.cloud.gds.cleaning.api.vo.DataSetVo;
+import com.cloud.gds.cleaning.utils.DataRuleUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.*;
+
+@SpringBootTest(classes = GdsCleaningApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DataRuleServiceTest {
+
+	@Autowired
+	DataRuleService dataRuleService;
+
+	@Test
+	public void selectPage() {
+		Map<String,Object> map = new HashMap<>();
+//		Page page = dataRuleService.selectPage(map);
+//		System.out.println(page.getRecords());
+	}
+
+	@Test
+	public void deleteById() {
+		System.out.println(dataRuleService.deleteById(5));
+	}
+
+	@Test
+	public void deleteByIds() {
+		Set<Long> ids = new HashSet<>();
+		ids.add(1L);
+		ids.add(2L);
+		ids.add(3L);
+		dataRuleService.deleteByIds(ids);
+
+	}
+
+	@Test
+	public void save() {
+		DataRuleVo dataRuleVo = new DataRuleVo();
+		dataRuleVo.setName("小二规则1214");
+		dataRuleService.save(dataRuleVo);
+	}
+
+	@Test
+	public void check(){
+		DataRule one = dataRuleService.selectById(4);
+		DataRule two = dataRuleService.selectById(5);
+		DataRuleVo oneVo = DataRuleUtils.po2Vo(one);
+		DataRuleVo twoVo = DataRuleUtils.po2Vo(two);
+		List<DataSetVo> listone = oneVo.getDetail();
+		List<DataSetVo> listtwo = twoVo.getDetail();
+		SortedMap<String,String> sorteOne = changeSortedMap(listone);
+		SortedMap<String,String> sorteTwo = changeSortedMap(listtwo);
+		Boolean flag = checkSortedMap(sorteTwo,sorteOne);
+		System.out.println(flag);
+
+	}
+
+	public static SortedMap<String,String> changeSortedMap(List<DataSetVo> dataSetVos){
+		SortedMap<String,String> sortedMap = new TreeMap<>();
+		for (DataSetVo vo : dataSetVos){
+			if (vo.getLabel() != "" || vo.getLabel().trim()!=""){
+				sortedMap.put(vo.getLabel(), vo.getLabel());
+			}
+		}
+		return sortedMap;
+	}
+
+	public static Boolean checkSortedMap(SortedMap<String,String> one,SortedMap<String,String> two){
+		Set<Map.Entry<String, String>> es = one.entrySet();
+		Iterator<Map.Entry<String,String>> it = es.iterator();
+		while (it.hasNext()){
+			Map.Entry<String,String> entry =  (Map.Entry<String,String>)it.next();
+			String k = (String)entry.getKey();
+			if (two.containsKey(k)){
+				two.remove(k);
+			}else {
+				return false;
+			}
+		}
+		if (two.size() > 0){
+			return false;
+		}
+		return true;
+	}
+}
