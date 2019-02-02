ebbd1291d228db52113b4536f664e3d92b611268
lolilijve <lolilijve@gmail.com>
2019-02-02 14:27:33 +0800

    -- 清洗over,但是国策爬取数据表id不是唯一主键存在一点问题

--- dips-modules/gds-cleaning/gds-cleaning-biz/src/test/java/com/cloud/gds/cleaning/service/GuoceJDBC.java
+++ dips-modules/gds-cleaning/gds-cleaning-biz/src/test/java/com/cloud/gds/cleaning/service/GuoceJDBC.java
@@ -43,7 +43,7 @@ public class GuoceJDBC {
 	@Test
 	public void guoceClean() {
 		List<Long> ids = doAnalysisService.getNoExactlySameDataIds(98L);
-		List<List<Long>> idLists = cutIds(ids);
+		List<List<Long>> idLists = cutIds(ids, 500);
 
 		AtomicInteger allNum = new AtomicInteger(idLists.size());
 		for (List<Long> idList : idLists) {
@@ -86,10 +86,6 @@ public class GuoceJDBC {
 				String name = rs.getString("title");
 				int is_deleted = rs.getInt("is_deleted");
 
-				//				// 输出数据
-				//				System.out.print("ID: " + id);
-				//				System.out.print(", 站点名称: " + name);
-				//				System.out.print("\n");
 				DataFieldValue fieldValue = new DataFieldValue();
 				fieldValue.setFieldId(97L);
 
@@ -97,8 +93,6 @@ public class GuoceJDBC {
 				gouceEntity.setId(id);
 				gouceEntity.setTitle(name);
 				gouceEntity.setIs_deleted(is_deleted);
-				//				System.out.println(rs.getRow());
-				//				System.out.println(JSON.toJSONString(gouceEntity));
 				fieldValue.setFieldValue(JSON.toJSONString(gouceEntity));
 				fieldValue.setCreateUser(0);
 				list.add(fieldValue);
@@ -106,8 +100,8 @@ public class GuoceJDBC {
 			}
 			dataFieldValueService.batchSave(list, 1000);
 			// 完成后关闭
-			rs.close();
-			stmt.close();
+			// rs.close();
+			// stmt.close();
 			myDataSource.releaseConnection(conn);
 		} catch (Exception e) {
 			// 处理 Class.forName 错误
@@ -187,31 +181,6 @@ public class GuoceJDBC {
 	}
 
 	/**
-	 * 弃用
-	 *
-	 * @param list
-	 * @param oneSize
-	 * @return
-	 */
-	@Deprecated
-	public boolean batchSave(List<Long> list, int oneSize) {
-		boolean flag = true;
-		List<Long> subList;
-		int currentNum = 0;
-		while (flag) {
-			if (list.size() > oneSize * (currentNum + 1)) {
-				subList = list.subList(currentNum * oneSize, oneSize * (currentNum + 1));
-			} else {
-				subList = list.subList(currentNum * oneSize, list.size());
-				flag = false;
-			}
-			move(subList);
-			currentNum++;
-		}
-		return true;
-	}
-
-	/**
 	 * 多线程打标签
 	 *
 	 * @throws Exception
@@ -219,40 +188,40 @@ public class GuoceJDBC {
 	public void MultiThreadLabel() throws Exception {
 
 		//查询未打标签的ids
-		Connection conn = null;
-		try {
-			conn = myDataSource.getConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		//todo jdbc操作 未打标签的ids
 		List<Long> ids = selectNoTagId();
-		myDataSource.releaseConnection(conn);
-		int oneSize = 100;
-		AtomicInteger currNum = new AtomicInteger();
-		analysisThreadPool.execute(() -> {
-			//根据ids分块查询数据
-			Connection connect = null;
-			try {
-				connect = myDataSource.getConnection();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			List<Long> idList = ids.subList(oneSize * currNum.get(), oneSize * (currNum.get() + 1));
-			currNum.getAndIncrement();
-			//todo jdbc操作
+		List<List<Long>> idLists = cutIds(ids, 100);
 
-			//打标签
-			//todo
-			TagRelation tagRelation = new TagRelation();
-			// 固定
-			tagRelation.setNode("gov_general_policy");
-			tagRelation.setType_id(4L);
+		for (List<Long> idList : idLists) {
+			analysisThreadPool.execute(() -> {
+				//根据ids分块查询数据
+				Connection conn = null;
+				Statement stmt;
+				ResultSet rs = null;
+				try {
+					conn = myDataSource.getConnection();
+					stmt = conn.createStatement();
+					//todo 请输入正确的sql
+					String sql = "SELECT * FROM x";
+					rs = stmt.executeQuery(sql);
+					while (rs.next()) {
+						//todo 请自建内部类并填充数据
 
-			//标签jdbc存储
-			//todo jdbc操作
-			myDataSource.releaseConnection(connect);
-		});
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+				//todo 打标签
+				TagRelation tagRelation = new TagRelation();
+				// 固定
+				tagRelation.setNode("gov_general_policy");
+				tagRelation.setType_id(4L);
+
+				//todo 对标签进行存储
+
+				myDataSource.releaseConnection(conn);
+			});
+		}
 	}
 
 	/**
@@ -261,8 +230,7 @@ public class GuoceJDBC {
 	 * @param ids
 	 * @return
 	 */
-	public List<List<Long>> cutIds(List<Long> ids) {
-		int oneSize = 500;
+	public List<List<Long>> cutIds(List<Long> ids, int oneSize) {
 		int currNum = 0;
 		List<List<Long>> lll = new ArrayList<>();
 		boolean flag = true;
@@ -282,57 +250,27 @@ public class GuoceJDBC {
 	}
 
 	public List<Long> selectNoTagId() {
+		List<Long> ids = new ArrayList<>();
 		Connection conn = null;
 		Statement stmt = null;
-		List<Long> ids = new ArrayList<>();
 		try {
-			// 注册 JDBC 驱动
-			Class.forName("com.mysql.jdbc.Driver");
-
-			// 打开链接
-			System.out.println("连接数据库...");
-			// conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-			// 执行查询
-			System.out.println(" 实例化Statement对象...");
+			conn = myDataSource.getConnection();
 			stmt = conn.createStatement();
-			String sql;
-			sql = "SELECT a.id FROM gov_policy_general a LEFT JOIN gov_tag_relation b ON a.id = b.relation_id WHERE b"
-				+ ".relation_id IS NULL AND a.examine_user_id = 2112 ";
+			String sql =
+				"SELECT a.id FROM gov_policy_general a LEFT JOIN gov_tag_relation b ON a.id = b.relation_id WHERE b"
+					+ ".relation_id IS NULL AND a.examine_user_id = 2112 ";
+			// 执行查询
 			ResultSet rs = stmt.executeQuery(sql);
-
 			// 展开结果集数据库
 			while (rs.next()) {
 				// 通过字段检索
 				Long id = rs.getLong("id");
 				ids.add(id);
-
-				// 输出数据
-				//				System.out.print("ID: " + id);
 			}
-			// 完成后关闭
-			rs.close();
-			stmt.close();
-			conn.close();
-		} catch (Exception e) {
-			// 处理 Class.forName 错误
+			// 完成后归还连接
+			myDataSource.releaseConnection(conn);
+		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			// 关闭资源
-			try {
-				if (stmt != null) {
-					stmt.close();
-				}
-			} catch (SQLException se2) {
-				// 什么都不做
-			}
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
 		}
 		System.out.println("selectNoTagId OVER!");
 		return ids;
