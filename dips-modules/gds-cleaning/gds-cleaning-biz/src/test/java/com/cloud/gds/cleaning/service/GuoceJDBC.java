0cd45194d643b759ff541058d5af4a8a40ad380d
lolilijve <lolilijve@gmail.com>
2019-02-27 16:14:43 +0800

    -- 时间填充

--- dips-modules/gds-cleaning/gds-cleaning-biz/src/test/java/com/cloud/gds/cleaning/service/GuoceJDBC.java
+++ dips-modules/gds-cleaning/gds-cleaning-biz/src/test/java/com/cloud/gds/cleaning/service/GuoceJDBC.java
@@ -351,7 +351,7 @@ public class GuoceJDBC {
 	@Test
 	public void MultiThreadGetTime1() throws Exception {
 		List<Long> ids = selectIds(
-			"SELECT id FROM gov_policy_general WHERE (publish_time like '%11:11:1%' or publish_time <='1900-1-2 00:00:00') and examine_user_id = "
+			"SELECT id FROM gov_policy_general WHERE (publish_time <='1900-1-2 00:00:00') and examine_user_id = "
 				+ "2158 and examine_status = 3");
 
 		AtomicInteger doNum = new AtomicInteger(ids.size());
@@ -384,7 +384,7 @@ public class GuoceJDBC {
 					// 创建 Pattern 对象
 					String pattern = ".*[^\u4e00-\u9fa5][\u4e00-\u9fa5]+?(机构|委|署|局|厅|处|部|室|委员会|行|院|台|中心|报|司|办|府)[^\u4e00-\u9fa5]*?(发文日期|生成日期)"
 						+ "?[\\s\\S]?[\\s]*?[^\\d](\\d+)年(\\d+)月(\\d+)日";
-					String pattern1 = ".*[^\\d](\\d+)年(\\d+)月(\\d+)日";
+					String pattern1 = ".*[^-|\\dA-z](\\d+)[年-](\\d+)[月-](\\d+)日?(?![-|\\d])";
 
 					Pattern p = Pattern.compile(pattern);
 					Pattern p1 = Pattern.compile(pattern1);
